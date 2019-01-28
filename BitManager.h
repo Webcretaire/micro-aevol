2f310363c27d370dd740b31107cd883aa1e13475
Laurent Turpin <laurent.turpin@insa-lyon.fr>
2019-01-28 15:34:54 +0100

    all BitManager is inline

--- BitManager.h
+++ BitManager.h
@@ -44,6 +44,17 @@ inline void def_bit(int32_t *dna, int pos, bool value) {
      : BIT_CLEAR(*(dna + CHUNCK_NUMBER(pos)), CHUNCK_OFFSET(pos)));
 }
 
-int32_t get_chunck(const int32_t *dna, int pos);
+inline int32_t get_chunck(const int32_t *dna, int pos) {
+    int cn = CHUNCK_NUMBER(pos);
+    int co = CHUNCK_OFFSET(pos);
+    if (co >= CHUNK_SIZE - MAX_INTERESTING_SIZE) {
+        int64_t holder = ((int64_t) (*(dna + cn + 1)) << CHUNK_SIZE);
+        holder += (*(dna + cn)) & 0b0000000000000000000000000000000011111111111111111111111111111111;
+
+        return (int32_t) (holder >> co);
+    }
+
+    return *(dna + cn) >> co;
+}
 
 #endif //PDC_MINI_AEVOL_BITMANAGER_H
