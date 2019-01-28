5e4f352ecab4b9d70c1b388d7fd1b53a1eed0d96
Julien EMMANUEL <JuEm0406@gmail.com>
2019-01-28 15:26:53 +0100

    BitManager inlining

--- BitManager.cpp
+++ BitManager.cpp
@@ -1,29 +1,7 @@
 #include <stdint-gcc.h>
 #include "BitManager.h"
 
-void set_bit(int32_t *dna, int pos) {
-    BIT_SET(*(dna + CHUNCK_NUMBER(pos)), CHUNCK_OFFSET(pos));
-}
-
-void clear_bit(int32_t *dna, int pos) {
-    BIT_CLEAR(*(dna + CHUNCK_NUMBER(pos)), CHUNCK_OFFSET(pos));
-}
-
-void flip_bit(int32_t *dna, int pos) {
-    BIT_FLIP(*(dna + CHUNCK_NUMBER(pos)), CHUNCK_OFFSET(pos));
-}
-
-int access_bit(const int32_t *dna, int pos) {
-    return BIT_CHECK(*(dna + CHUNCK_NUMBER(pos)), CHUNCK_OFFSET(pos));
-}
-
-void def_bit(int32_t *dna, int pos, bool value) {
-    value
-    ? BIT_SET(*(dna + CHUNCK_NUMBER(pos)), CHUNCK_OFFSET(pos))
-    : BIT_CLEAR(*(dna + CHUNCK_NUMBER(pos)), CHUNCK_OFFSET(pos));
-}
-
-int32_t get_chunck(int32_t *dna, int pos) {
+int32_t get_chunck(const int32_t *dna, const int pos) {
     int cn = CHUNCK_NUMBER(pos);
     int co = CHUNCK_OFFSET(pos);
     if (co >= CHUNK_SIZE - MAX_INTERESTING_SIZE) {
