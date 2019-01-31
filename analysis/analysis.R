76480884abb5259f2837afa2911757df77567991
Julien EMMANUEL <JuEm0406@gmail.com>
2019-01-31 20:40:09 +0100

    refactoring

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -10,6 +10,7 @@ setwd(dirname(getActiveDocumentContext()$path))
 
 source("util.R")
 source("exports.R")
+source("plots.R")
 
 data <- read.csv("data.csv")
 
@@ -30,75 +31,15 @@ names(data) <- c(
 g_size <- 32000
 
 filtered_data <- subset(data, genomeSize == g_size)
-
-gathered_data <-
-  gather(filtered_data,
-         algorithm,
-         time,
-         starts_with('ompMeasure'))
-
-plot <-
-  ggplot(gathered_data,
-         aes(x = numThreads,
-             y = time,
-             fill = algorithm)) +
-  facet_wrap(gridSize ~ mutationRate) +
-  scale_x_continuous(trans = 'log2') +
-  scale_y_log10(labels = trans_format("log10", math_format(10 ^ .x))) +
-  geom_bar(stat = "identity",
-           position = position_dodge(),
-           colour = "black") +
-  theme(legend.position = "bottom") +
-  guides(fill = guide_legend(nrow = 4))
-
+plot <- plot.parallel(filtered_data)
 export.all("fort", plot)
 
 filtered_data <- subset(data, seqMeasureOneFor != -1)
-
-gathered_data <-
-  gather(filtered_data,
-         algorithm,
-         time,
-         starts_with('seqMeasure'))
-
-plot <-
-  ggplot(gathered_data,
-         aes(x = genomeSize,
-             y = time,
-             fill = algorithm)) +
-  facet_wrap(gridSize ~ mutationRate) +
-  scale_x_log10() +
-  scale_y_log10(labels = trans_format("log10", math_format(10 ^ .x))) +
-  geom_bar(stat = "identity",
-           position = position_dodge(),
-           colour = "black") +
-  theme(legend.position = "bottom") +
-  guides(fill = guide_legend(nrow = 3))
-
+plot <- plot.sequential(filtered_data)
 export.all("seq", plot)
 
 filtered_data <- subset(data, genomeSize == 1000 * numThreads)
-
-gathered_data <-
-  gather(filtered_data,
-         algorithm,
-         time,
-         starts_with('ompMeasure'))
-
-plot <-
-  ggplot(gathered_data,
-         aes(x = numThreads,
-             y = time,
-             fill = algorithm)) +
-  facet_wrap(gridSize ~ mutationRate) +
-  scale_x_continuous(trans = 'log2') +
-  scale_y_log10(labels = trans_format("log10", math_format(10 ^ .x))) +
-  geom_bar(stat = "identity",
-           position = position_dodge(),
-           colour = "black") +
-  theme(legend.position = "bottom") +
-  guides(fill = guide_legend(nrow = 4))
+plot <- plot.parallel(filtered_data)
+export.all("faible", plot)
 
 print(plot)
-
-export.all("faible", plot)
